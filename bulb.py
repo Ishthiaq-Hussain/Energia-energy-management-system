6e5e61979ef9e63eb76483e7b3a186317d838caa
Ishthiaq-Hussain <54953214+Ishthiaq-Hussain@users.noreply.github.com>
2020-08-17 19:16:03 +0530

    Add files via upload

---
+++ bulb.py
@@ -0,0 +1,26 @@
+import serial
+import datetime,time
+import csv
+csv_filename='bulb.csv'
+
+
+ser=serial.Serial('/dev/ttyACM0',9600)
+while True:
+    with open(csv_filename, 'a') as myfile:
+        wr = csv.writer(myfile)
+        p=ser.readline()
+        data=p.split()
+        current_date=datetime.datetime.now()
+        current_voltage=data[0]
+        current_current=data[1]
+        mydata=[current_date,current_voltage,current_current]
+        wr.writerow(mydata)
+        print(mydata)
+        myfile.close()
+
+
+
+
+
+
+
